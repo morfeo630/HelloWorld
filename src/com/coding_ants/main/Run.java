8807b969a6cf3ae34206e3c32ad0be3acd6b0539
Valter <vjagnj@coding-ants.com>
2016-07-14 15:52:10 +0200

    New Eclipse project HelloWorld!
    Signed-off-by: Valter <vjagnj@coding-ants.com>

---
+++ src/com/coding_ants/main/Run.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.coding_ants.main;
+
+/**
+ * @author vjagnj
+ *
+ */
+public class Run {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
