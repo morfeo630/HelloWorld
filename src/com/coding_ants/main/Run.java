2e6ee8424f2df786fdfbb33cbc6fdc964d6dbe80
Valter <vjagnj@coding-ants.com>
2016-07-14 16:10:13 +0200

    Added a nice printLine :)
    Signed-off-by: Valter <vjagnj@coding-ants.com>

--- src/com/coding_ants/main/Run.java
+++ src/com/coding_ants/main/Run.java
@@ -13,7 +13,7 @@ public class Run {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("What else than Hello World exists? :)");
 
 	}
 
